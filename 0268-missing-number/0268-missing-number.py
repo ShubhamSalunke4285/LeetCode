27d13e61331dae03e3b1568d207f1285663c6dde
Shubham Salunke <shubhamsalunke4285@gmail.com>
2025-06-01 21:16:41 -0700

    Time: 0 ms (100%), Space: 13.5 MB (30.68%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,8 +1,11 @@
 class Solution(object):
     def missingNumber(self, nums):
-        for i in range(len(nums)+1):
-            if i in nums:
-                continue
-            else:
-                return i
+        # for i in range(len(nums)+1):
+        #     if i in nums:
+        #         continue
+        #     else:
+        #         return i
         
+        n = len(nums)
+        total = n * (n + 1) // 2
+        return total - sum(nums)
