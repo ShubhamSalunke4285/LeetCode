bb280752981ac950285d8db3a503e5cdfdfee8ba
Shubham Salunke <shubhamsalunke4285@gmail.com>
2025-06-04 20:01:56 -0700

    Time: 1 ms (77.83%), Space: 14.6 MB (19.82%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def singleNumber(self, nums):
-        result = 0
-        for num in nums:
-            result ^= num  # Apply XOR step-by-step
-        return result
-        # return 2*sum(set(nums))-sum(nums)
+        # result = 0
+        # for num in nums:
+        #     result ^= num  # Apply XOR step-by-step
+        # return result
+        return 2*sum(set(nums))-sum(nums)
         
 
 
