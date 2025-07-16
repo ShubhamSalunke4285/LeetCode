33cf1e70cfa6b1c337cdea4d636b2f019638b3f4
Shubham Salunke <shubhamsalunke4285@gmail.com>
2025-07-15 19:31:09 -0700

    Time: 728 ms (57.81%), Space: 18.3 MB (86.01%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        res = []
+        n = len(nums)
+
+        for i in range(n):
+            # Skip duplicate values for the first element
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            # Two-pointer approach
+            left, right = i + 1, n - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    res.append([nums[i], nums[left], nums[right]])
+
+                    # Skip duplicates for second and third element
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                    # Move both pointers inward
+                    left += 1
+                    right -= 1
+
+                elif total < 0:
+                    left += 1  # Increase the sum
+                else:
+                    right -= 1  # Decrease the sum
+
+        return res
+                
+
+        
