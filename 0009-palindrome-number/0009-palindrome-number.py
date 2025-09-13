b86e75196d7672ba831bcd2e0a2f7a889647cb93
Shubham Salunke <shubhamsalunke4285@gmail.com>
2025-09-12 19:11:21 -0700

    Time: 3 ms (93.64%), Space: 12.5 MB (51.94%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        num = x
+        result = 0
+        while num>0:
+            id = num % 10
+            result = (result*10) + id
+            num = num//10
+        return x == result
+        
