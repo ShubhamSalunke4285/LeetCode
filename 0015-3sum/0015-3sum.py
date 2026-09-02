616d2f11f3bfbfa061ab7868d32c4b0ad707150d
Shubham Salunke <shubhamsalunke4285@gmail.com>
2026-09-01 20:23:11 -0700

    Time: 728 ms (73.66%), Space: 18.2 MB (81.21%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,35 +1,40 @@
 class Solution(object):
     def threeSum(self, nums):
         nums.sort()
-        res = []
-        n = len(nums)
+        target = []
 
-        for i in range(n):
-            # Skip duplicate values for the first element
-            if i > 0 and nums[i] == nums[i - 1]:
+        # -4,-4,-1,-1,0,1,1,2
+        for i in range(len(nums)-2):
+            if nums[i]>0:
+                break
+
+            if i>0 and nums[i] == nums[i-1]:
                 continue
+
+            left = i+1
+            right = len(nums)-1
             
-            # Two-pointer approach
-            left, right = i + 1, n - 1
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-
-                if total == 0:
-                    res.append([nums[i], nums[left], nums[right]])
-
-                    # Skip duplicates for second and third element
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-
-                    # Move both pointers inward
-                    left += 1
-                    right -= 1
-
-                elif total < 0:
-                    left += 1  # Increase the sum
+            while left<right:
+                if nums[i] + nums[left] + nums[right] < 0:
+                    left +=1
+                elif nums[i] + nums[left] + nums[right] > 0:
+                    right -=1
                 else:
-                    right -= 1  # Decrease the sum
+                    target.append([nums[i], nums[left], nums[right]])
 
-        return res
+                    left +=1
+                    right -=1
+
+                    while left<right and nums[left] == nums[left-1]:
+                        left+=1
+
+                    while left<right and nums[right] == nums[right+1]:
+                        right-=1
+
+        return target
+
+
+
+
+
+        
